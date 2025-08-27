753b150c68883c84a761049b81638f3cad33a16b
Stanislav Turchinskiy <s.turchinskiy@yandex.ru>
2025-08-27 19:24:50 +0300

    errors

--- internal/sqlrequests/get_balance.sql
+++ internal/sqlrequests/get_balance.sql
@@ -1,6 +1,8 @@
 select
-    (
-        select
-            sum(sum)
-        from
-            loyaltysystem.balances where user_id = (select id from loyaltysystem.users where login = $1)) as current
+    coalesce(
+            (
+                select
+                    sum(sum)
+                from
+                    loyaltysystem.balances where user_id = (select id from loyaltysystem.users where login = 'test'))
+        , 0) as current
