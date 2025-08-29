956380c804adff1322f1f77ef5b7d70198ca7d2a
Stanislav Turchinskiy <s.turchinskiy@yandex.ru>
2025-08-29 23:35:00 +0300

    error correction

--- internal/accrualservice/httpresty.go
+++ internal/accrualservice/httpresty.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 )
 
-type ReportMetricsHTTPResty struct {
+type HTTPResty struct {
 	client *resty.Client
 	url    string
 }
@@ -19,15 +19,15 @@ type AccrualGetter interface {
 	GetAccrual(url string) (result *models.AccrualData, err error)
 }
 
-func NewHTTPResty(url string) *ReportMetricsHTTPResty {
+func NewHTTPResty(url string) *HTTPResty {
 
-	return &ReportMetricsHTTPResty{
+	return &HTTPResty{
 		client: resty.New(),
 		url:    url,
 	}
 }
 
-func (r *ReportMetricsHTTPResty) GetAccrual(numberOrder string) (result *models.AccrualData, err error) {
+func (r *HTTPResty) GetAccrual(numberOrder string) (result *models.AccrualData, err error) {
 
 	request := r.client.R()
 
@@ -46,6 +46,8 @@ func (r *ReportMetricsHTTPResty) GetAccrual(numberOrder string) (result *models.
 		return nil, err
 	}
 
+	logger.Log.Debugln(zap.String("body", string(resp.Body())))
+	
 	err = easyjson.Unmarshal(resp.Body(), result)
 	if err != nil {
 		logger.Log.Info("error encoding response", zap.Error(err))
